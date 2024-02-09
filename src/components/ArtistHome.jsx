fbfe41353a9b8d29a96d2763f7e9daf0c2ed0b14
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-02-09 23:13:58 +0100

    creazione artist home

---
+++ src/components/ArtistHome.jsx
@@ -0,0 +1,8 @@
+const ArtistHome = () => {
+  return (
+    <div>
+      <h2 className="text-white mt-20 ml-20">ARTIST__</h2>
+    </div>
+  )
+}
+export default ArtistHome
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
+import ArtistHome from "../components/ArtistHome"
 import CarouselHero from "../components/CarouselHero"
 
 const HomePage = () => {
-  return <CarouselHero />
+  return (
+    <>
+      <CarouselHero />
+      <ArtistHome />
+    </>
+  )
 }
 
 export default HomePage

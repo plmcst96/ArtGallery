99897c20e718967c345a2f075e743127a28ebba4
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-03-01 17:19:45 +0100

    curator profile + crud

--- src/components/CuratorRequest.jsx
+++ src/components/CuratorRequest.jsx
@@ -1,7 +1,26 @@
+import { Typography } from "@material-tailwind/react"
+import { ArtistRequest } from "./ArtistRequest"
+import { WorkRequest } from "./WorkRequest"
+import { ExhibitionRequest } from "./ExhibitionRequest"
+
 export const CuratorRequest = () => {
   return (
-    <div>
-      <div></div>
+    <div className="mt-20">
+      <div className="mx-10">
+        <Typography variant="h3" className="text-[#e71b82]">
+          CURATOR__
+        </Typography>
+        <Typography color="white">
+          On this page you can send requests to add an artist or a work of art
+          related to an artist. <br /> You can also propose exhibitions to
+          publicize your new artist!
+        </Typography>
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-4 mx-10 gap-10 mt-20">
+        <ArtistRequest />
+        <WorkRequest />
+        <ExhibitionRequest />
+      </div>
     </div>
   )
 }
---
+++ src/components/ExhibitionRequest.jsx
@@ -0,0 +1,199 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Input,
+  Textarea,
+  Typography,
+} from "@material-tailwind/react"
+import { useState } from "react"
+
+export const ExhibitionRequest = () => {
+  const [isFormVisible, setIsFormVisible] = useState(false)
+  const work = JSON.parse(localStorage.getItem("workRequest"))
+  const [formData, setFormData] = useState({
+    uuid: uuidv4(),
+    title: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    amount: "",
+    openHour: "",
+    artistWork: work.uuid,
+  })
+
+  const toggleFormVisibility = () => {
+    setIsFormVisible(!isFormVisible)
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleFormSubmit = () => {
+    // Save form data to local storage
+    const requestId = uuidv4()
+    localStorage.setItem("workRequest", JSON.stringify(formData))
+    // Optionally, you can clear the form data after submission
+    setFormData({
+      uuid: requestId,
+      title: "",
+      startDate: "",
+      endDate: "",
+      description: "",
+      amount: "",
+      openHour: "",
+      artistWork: "",
+    })
+    toggleFormVisibility()
+  }
+
+  return (
+    <div>
+      <div>
+        <Card className="mt-6 w-full">
+          <CardBody>
+            <Typography variant="h5" className="mb-2 text-[#e71b82]">
+              Exhibition__
+            </Typography>
+            <Typography>Add a new Exhibition to the list!</Typography>
+          </CardBody>
+          <CardFooter className="pt-0">
+            <Button onClick={toggleFormVisibility} className="rounded-full">
+              Add request
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      {isFormVisible && (
+        <div
+          className="flex justify-start item-center mt-20"
+          style={{ width: "780px" }}
+        >
+          <Card color="transparent" className="">
+            <Typography variant="h4" className="text-[#e71b82]">
+              New Exhibition__
+            </Typography>
+            <Typography color="gray" className="mt-1 font-normal">
+              Oh ok! You want to add a new Art Work? Alerady!
+            </Typography>
+            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+              <div className="mb-1 flex flex-col gap-6">
+                <Typography variant="h6" color="white" className="-mb-3">
+                  Title
+                </Typography>
+                <Input
+                  size="lg"
+                  name="title"
+                  value={formData.title}
+                  onChange={handleInputChange}
+                  placeholder="name"
+                  className=" !border-t-white-200 focus:!border-t-gray-900"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+                <Typography variant="h6" color="white" className="-mb-3">
+                  Start Date
+                </Typography>
+                <Input
+                  size="lg"
+                  name="startDate"
+                  value={formData.startDate}
+                  onChange={handleInputChange}
+                  placeholder="surname"
+                  className=" !border-t-white-200 focus:!border-t-gray-900"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+                <Typography variant="h6" color="white" className="-mb-3">
+                  End Date
+                </Typography>
+                <Input
+                  size="lg"
+                  name="endDate"
+                  value={formData.endDate}
+                  onChange={handleInputChange}
+                  placeholder="surname"
+                  className=" !border-t-white-200 focus:!border-t-gray-900"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+                <Typography variant="h6" color="white" className="-mb-3">
+                  Open Hour
+                </Typography>
+                <Input
+                  type="text"
+                  name="openHour"
+                  value={formData.openHour}
+                  onChange={handleInputChange}
+                  size="lg"
+                  placeholder="date"
+                  className=" !border-t-white-200 focus:!border-t-gray-900"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+                <Typography variant="h6" color="white" className="-mb-3">
+                  Description
+                </Typography>
+                <Textarea
+                  type="text"
+                  name="description"
+                  value={formData.description}
+                  onChange={handleInputChange}
+                  size="lg"
+                  placeholder="history"
+                  className=" !border-t-white-200 focus:!border-t-gray-900"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+                <Typography variant="h6" color="white" className="-mb-3">
+                  Amount
+                </Typography>
+                <Input
+                  type="text"
+                  name="amount"
+                  value={formData.amount}
+                  onChange={handleInputChange}
+                  size="lg"
+                  placeholder="date"
+                  className=" !border-t-white-200 focus:!border-t-gray-900"
+                  labelProps={{
+                    className: "before:content-none after:content-none",
+                  }}
+                />
+
+                {/* Add other input fields similarly */}
+              </div>
+              <Button
+                className="mt-6 bg-[#e71b82] rounded-full"
+                fullWidth
+                onClick={handleFormSubmit}
+              >
+                Add Art Work
+              </Button>
+            </form>
+          </Card>
+        </div>
+      )}
+    </div>
+  )
+}
+
+// Function to generate UUID (Universally Unique Identifier)
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
